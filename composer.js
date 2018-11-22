b8e083c9c130a63d2724316823c0c1180461bb8f
Dominique Hazael-Massieux <dom@w3.org>
2018-11-22 18:42:56 +0100

    Create browser-based composer of media elements and animations

---
+++ composer.js
@@ -0,0 +1,119 @@
+const canvas = document.getElementById('composer');
+const ctx = canvas.getContext('2d');
+const startBtn = document.getElementById('start');
+
+
+const loadAsset = (type, path) => {
+  let el, loaded;
+  if (type === 'a' || type === 'v' || type === 'i') {
+    el = {
+      'a': new Audio(),
+      'v': document.createElement('video'),
+      'i': new Image()
+    }[type];
+    el.src = path;
+    if (type === 'v' || type === 'i') {
+      // TODO: set width & height
+      el.width = canvas.width;
+      el.style.width = canvas.width;
+      el.style.objectFit = 'contain';
+    }
+    if (type === 'i') {
+      loaded = new Promise((res, rej) => el.onload = res);
+    } else {
+      loaded = new Promise((res, rej) => el.onloadedmetadata = res);
+    }
+  } else if (type === 't') {
+    el = document.createElement('canvas');
+    el.width = canvas.width;
+    el.height = canvas.height;
+    loaded = runningTitle(path, el);
+  }
+  return {el, loaded};
+};
+
+
+
+fetch("script").then(r => r.text())
+  .then(script => {
+    const [assetDefs, timelineDefs] = script.split("\n\n\n");
+    const allLoaded = [];
+    const assets = assetDefs.split('\n').reduce((acc, def) => {
+      const [id, filename] = def.split(':');
+      const {el, loaded} = loadAsset(id.slice(0, 1), filename);
+      allLoaded.push(loaded);
+      acc[id] = {filename, el};
+      return acc;
+    }, {});
+    let assetTimelines = Object.keys(assets).reduce((acc, x) => { acc[x] = 0; return acc;}, {});
+    const convertTimeboundaryDef = (boundary, offset = 0) => {
+      let comp;
+      if (comp = boundary.match(/(\+?)([0-9]{2}):([0-9]{2}):([0-9]{2}),([0-9]{3})/)) {
+        const [, sign, hours, minutes, seconds, ms] = comp.map((c,i) => i > 1 ? parseInt(c, 10) : c);
+        return (sign === '+' ? offset : 0) + hours * 3600* 1000 + minutes * 60 * 1000 + seconds * 1000 + ms;
+      } else if (comp = boundary.match(/([\+>]?)([aitv][0-9]+)/)) {
+        const [, sign, id] = comp;
+        if (sign === '+') {
+          // nothing to do, offset is already set
+        } else if (sign === '>') {
+          offset -= assetTimelines[id];
+        } else {
+          offset = 0;
+        }
+        return offset + assets[id].el.duration * 1000;
+      }
+    };
+
+    Promise.all(allLoaded).then(() => {
+      let prevTime = 0;
+      const timeline = [];
+      timelineDefs.split('\n\n').forEach(block => {
+        const [timing, ...commandLines] = block.split('\n');
+        const [start, end] = timing.split('->').map(b => convertTimeboundaryDef(b, prevTime));
+        prevTime = end;
+
+        const commands = commandLines.filter(x => x).map(c => { [verb, target, ...options] = c.split(' '); return {verb, target, options, done: false};})
+        commands.forEach(({verb, target}) => {
+          if (verb === 'play') {
+            assetTimelines[target] += end - start;
+          }
+        });
+
+        timeline.push({start, end, commands});
+      });
+      let start;
+      const end = Math.max.apply(null, timeline.map(t => t.end));
+      const render = () => {
+        const now = performance.now() - start;
+        timeline.filter(t => t.start <= now && t.end >= now)
+          .forEach(t => t.commands.forEach(command => {
+            const {verb, target} = command;
+            const type = target.slice(0, 1);
+            const targetEl = assets[target].el;
+            if (!command.done) {
+              if (verb === 'pause') {
+                targetEl.pause();
+              } else if (verb === 'play') {
+                if (type !== 'i')
+                  targetEl.play();
+              }
+              command.done = true;
+            }
+            if (type === 'v' || type === 't' || type === 'i') {
+              ctx.drawImage(targetEl, 0, 0, canvas.width, canvas.height);
+            } else {
+
+            }
+          })
+                  );
+        if (now <= end) {
+          requestAnimationFrame(render);
+        }
+      }
+      startBtn.disabled = false;
+      startBtn.onclick = () => {
+        start = performance.now();
+        requestAnimationFrame(render);
+      };
+  });
+  });
---
+++ one-slider.js
@@ -0,0 +1,96 @@
+function runningTitle(filename, cv) {
+  const ctx = cv.getContext("2d");
+  const charDelay = 50;
+  const lineDelay = 50;
+  const endDelay = 1000;
+  const fontSize = 60;
+  const bgColor = 'transparent';
+  const lineBgColor = 'rgba(255, 255, 255, 0.75)';
+  const defaultColor = '#005A9C';
+  const strokeColor = 'black';
+  const highlightColor = '#CB6628';
+
+  const removeSpecialChar = x => x !== '*';
+
+  let text, start;
+  cv.currentTime = 0;
+  cv.videoWidth = cv.width;
+  cv.videoHeight = cv.height;
+
+  const loaded = new Promise((res, rej) => {
+    fetch(filename).then(r => r.text())
+      .then(loadedtext => {
+        text = loadedtext;
+        cv.duration = (text.split("\n").length * lineDelay + text.split("\n").reduce((total, l) => total + [...l].filter(removeSpecialChar).length * charDelay, 0) + endDelay) / 1000;
+        res();
+      });
+  });
+
+  function drawLineUpTo(line, i, withCaret, y) {
+    const rawchars = [...line];
+    const chars = rawchars.filter(removeSpecialChar);
+    ctx.fillText(chars.slice(0, i + 1).join('') +  (withCaret ? '_' : ''), 50, y);
+    ctx.strokeText(chars.slice(0, i + 1).join(''), 50, y);
+
+    // write highlights
+    const tmp = ctx.fillStyle;
+    ctx.fillStyle = highlightColor;
+    let rawCur = 0, cur = 0;
+    while (cur <= i && rawCur < rawchars.length) {
+      if (rawchars[rawCur] == '*') {
+        const {width: offset} = ctx.measureText(chars.slice(0,cur).join(''));
+        rawCur++;
+        ctx.fillText(chars[cur], 50 + offset, y);
+      } else {
+        cur++;
+        rawCur++;
+      }
+    }
+    ctx.fillStyle = tmp;
+  }
+
+  function clearRect(x, y, w, h) {
+    let tmp = ctx.fillStyle;
+    ctx.fillStyle = bgColor;
+    // clear (transp) vs fill (solid)
+    if (!bgColor || bgColor === 'transparent')
+      ctx.clearRect(x, y, w, h);
+    if (lineBgColor) {
+      ctx.fillStyle = lineBgColor;
+      ctx.fillRect(x, y, w, h);
+    }
+    ctx.fillStyle = tmp;
+  }
+
+  function drawLines(lines, i, j) {
+    cv.currentTime = performance.now() - start;
+    if (j >= lines.length)
+      return setTimeout(() => {
+        cv.currentTime = performance.now() - start;
+        ctx.clearRect(0, 0, cv.width, cv.height);
+      }, 1000);
+    const y = 200 + j*150;
+    const linelength = [...lines[j]].length;
+    if (i >= linelength) {
+      return setTimeout(() => {
+        clearRect(50, 200 + j*150 - fontSize, cv.width - 50, fontSize*1.5);
+        drawLineUpTo(lines[j], i, false, y);
+        drawLines(lines, 0, j + 1);
+      }, lineDelay);
+    }
+    clearRect(50, y - fontSize, cv.width - 50, fontSize*1.5);
+    drawLineUpTo(lines[j], i, true,  y);
+    setTimeout(() => drawLines(lines, i + 1, j), charDelay);
+  }
+
+  cv.play = function run() {
+    start = performance.now();
+    ctx.clearRect(0, 0, cv.width, cv.height);
+    const lines = text.split("\n");
+    ctx.font = '' + fontSize + 'px BaseTwelveSansB,Helvetica Neue, sans-serif';
+    ctx.fillStyle = defaultColor;
+    ctx.strokeStyle = strokeColor + ' 4px';
+    drawLines(lines, 0, 0);
+  }
+  return loaded;
+}
